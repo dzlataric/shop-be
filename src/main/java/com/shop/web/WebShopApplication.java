f3464c7e610f9f92020120714bc5d68d68daf139
dzlataric <dejanzlataric@outlook.com>
2017-07-28 13:53:33 +0200

    Initial spring boot with dependencies

---
+++ src/main/java/com/shop/web/WebShopApplication.java
@@ -0,0 +1,19 @@
+package com.shop.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebShopApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebShopApplication.class);
+	}
+}

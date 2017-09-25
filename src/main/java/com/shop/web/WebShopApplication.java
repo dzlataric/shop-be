c20ee457ba78b17bd99395bf7b37ed663e4af6ee
dzlataric <dejanzlataric@outlook.com>
2017-09-25 21:11:47 +0200

    Added eureka discovery

--- src/main/java/com/shop/web/WebShopApplication.java
+++ src/main/java/com/shop/web/WebShopApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class WebShopApplication extends SpringBootServletInitializer {
+public class WebShopApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebShopApplication.class, args);
 	}
-	
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(WebShopApplication.class);
